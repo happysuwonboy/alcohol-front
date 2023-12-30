7f83e6c6c14740ab1235abdec2146703efe659af
syh <yhoonsong96@gmail.com>
2023-12-30 20:53:01 +0900

    [Add] Header UI 작업 시작

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,5 +1,42 @@
+import { FaSearch } from "react-icons/fa";
+import { RiShoppingBagLine } from "react-icons/ri";
+import { Link } from 'react-router-dom';
+
 export default function Header() {
+    const user = {user_id: null, isAdmin : false}
+
     return(
-        <div>header</div>
+        <div className='pc_header_wrapper'>
+            <div className='header_top'>
+                <div className='main_logo'>
+                    <img src="assets/images/main-logo.png" alt="" />
+                </div>
+                <form className='header_search' action="">
+                    <label className='hidden_label' htmlFor="">검색창</label>
+                    <FaSearch />
+                    <input type="text" placeholder='무엇을 찾고 계신가요?'/>
+                </form>
+                <div className='header_user_menu'>
+                    { !user.user_id ? 
+                        <Link className='header_log login_btn' to='/login'> 로그인/회원가입  &gt; </Link>
+                     : null}
+                    { user.user_id ?
+                        <>
+                        <Link to='/mypage' className='mypage_link'>
+                            {user.user_id} 님 &gt;
+                        </Link>
+                        <button className='header_log logout_btn' to='/login'> 로그아웃 </button>
+                        </>
+                    : null}
+                    <button className='cart_btn'>
+                        <RiShoppingBagLine />
+                    </button>
+                </div>
+
+            </div>
+            <ul className='header_navbar'>
+                
+            </ul>
+        </div>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,27 @@
 export default function Home() {
     return(
-        <div>home</div>
+        <div>
+            <div>contents1...</div>
+            <div>contents2...</div>
+            <div>contents3...</div>
+            <div>contents4...</div>
+            <div>contents5...</div>
+            <div>contents6...</div>
+            <div>contents7...</div>
+            <div>contents8...</div>
+            <div>contents9...</div>
+            <br /><br /><br /><br /><br /><br /><br /><br />
+            <br /><br /><br /><br /><br /><br /><br /><br />
+            <br /><br /><br /><br /><br /><br /><br /><br />
+            <br /><br /><br /><br /><br /><br /><br /><br />
+            <br /><br /><br /><br /><br /><br /><br /><br />
+            <br /><br /><br /><br /><br /><br /><br /><br />
+            <br /><br /><br /><br /><br /><br /><br /><br />
+            <br /><br /><br /><br /><br /><br /><br /><br />
+            <br /><br /><br /><br /><br /><br /><br /><br />
+            <br /><br /><br /><br /><br /><br /><br /><br />
+            <br /><br /><br /><br /><br /><br /><br /><br />
+            <br /><br /><br /><br /><br /><br /><br /><br />
+        </div>
     );
 }
