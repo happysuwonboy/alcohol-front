d7c948bcf37f6e0e379e124725858463b5927802
최유진 <tneod2@icloud.com>
2024-01-05 15:02:22 +0900

    [Add] 특정 페이지에서 footer 안보이게 하는 코드 추가

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Outlet } from 'react-router-dom';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 import TopButton from './components/common/TopButton';
+import { useLocation } from 'react-router-dom';
 
 function App() {
+  const location = useLocation();
+  const invisible = ['/cart','/payment','/payment/receipt'].includes(location.pathname);
+  
   return (
     <>
     <Header/>
     <Outlet/>
-    <Footer/>
+    {!invisible && <Footer/>}
     <TopButton />
     </>
   );
