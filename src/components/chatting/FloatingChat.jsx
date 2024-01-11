e46dceeb759188958ab256f0235e91002e753dca
syh <yhoonsong96@gmail.com>
2024-01-12 00:49:36 +0900

    [Add] 채팅 - 소켓 연결 구현, 유저 측 채팅 기능 구현 중

--- src/components/chatting/FloatingChat.jsx
+++ src/components/chatting/FloatingChat.jsx
@@ -1,101 +1,123 @@
 import { useEffect, useRef, useState } from 'react';
 import { IoChatbubbleEllipsesSharp } from 'react-icons/io5';
-import Draggable from "react-draggable";
+import 'react-chat-elements/dist/main.css';
+import Draggable from 'react-draggable';
+import UserChat from './UserChat';
 
 export default function FloatingChat() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [isMobileDevice, setIsMobileDevice] = useState(window.innerWidth >= 601 ? false : true)
-  const [isDragging, setIsDragging] = useState(false)
-  const btnRef = useRef(null)
+  const [showChat, setShowChat] = useState(false)
+  const [isMobileDevice, setIsMobileDevice] = useState(
+    window.innerWidth >= 601 ? false : true
+  );
+  let [unReadCount, setUnReadCount] = useState(0)
+  const [isDragging, setIsDragging] = useState(false);
+  const btnRef = useRef(null);
   const contentRef = useRef(null);
-
 
   const updateBtnPosition = () => {
     const buttonRect = btnRef.current.getBoundingClientRect();
-
-    let newX = position.x
-    let newY = position.y
-
-    if (buttonRect.left < 0) { // 화면 왼쪽 뚫은 경우
-      newX = -(window.innerWidth - 105)
+    let newX = position.x;
+    let newY = position.y;
+    if (buttonRect.left < 0) {
+      // 화면 왼쪽 뚫은 경우
+      newX = -(window.innerWidth - 105);
     }
 
-    if (buttonRect.left + 50 > window.innerWidth) { // 화면 오른쪽 뚫은 경우
-      newX = 0
+    if (buttonRect.left + 50 > window.innerWidth) {
+      // 화면 오른쪽 뚫은 경우
+      newX = 0;
     }
 
-    if (buttonRect.top < 0) { // 화면 위쪽 뚫은 경우
-      newY = -(window.innerHeight-145)
+    if (buttonRect.top < 0) {
+      // 화면 위쪽 뚫은 경우
+      newY = -(window.innerHeight - 145);
     }
 
-    if (buttonRect.top+50 > window.innerHeight) { // 화면 아래쪽 뚫은 경우
+    if (buttonRect.top + 50 > window.innerHeight) {
+      // 화면 아래쪽 뚫은 경우
       newY = 30;
     }
 
-    setPosition({x : newX, y : newY})
-  }
+    setPosition({ x: newX, y: newY });
+  };
 
   const handleResize = () => {
     if (window.innerWidth >= 601) {
-      setIsMobileDevice(false)
+      setIsMobileDevice(false);
     } else {
-      setIsMobileDevice(true)
+      setIsMobileDevice(true);
     }
     updateBtnPosition();
-  }
+  };
 
-
-  useEffect(()=>{
-    window.addEventListener('resize',handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-  },[window.innerWidth])
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [window.innerWidth]);
 
   const toggleShowContent = (e) => {
-    if (isDragging) return
+    if (isDragging) return;
     if (Array.from(contentRef.current?.classList).includes('show')) {
-      contentRef.current?.classList.remove('show')
+      contentRef.current?.classList.remove('show');
+      setShowChat(false)
     } else {
-      contentRef.current?.classList.add('show')
+      contentRef.current?.classList.add('show');
+      setShowChat(true)
+      setUnReadCount(0)
     }
-  }
+  };
 
   const handleDrag = (e, data) => {
-    contentRef.current.style.transition = 'all 0s'
-    setPosition({x : data.x, y : data.y})
-    setIsDragging(true)
-  }
+    contentRef.current.style.transition = 'all 0s';
+    setPosition({ x: data.x, y: data.y });
+    setIsDragging(true);
+  };
 
-  const handleDragStop = () => setTimeout(() => {
-    setIsDragging(false)
-    contentRef.current.style.transition = 'all .5s'
-    updateBtnPosition();
-  }, 100);
-
+  const handleDragStop = () =>
+    setTimeout(() => {
+      setIsDragging(false);
+      contentRef.current.style.transition = 'all .5s';
+      updateBtnPosition();
+    }, 100);
 
 
   return (
     <>
       <div className="float_chat_container">
         <Draggable
-          position={{x : position.x, y : position.y}}
-          handle='.float_chat_btn'
+          position={{ x: position.x, y: position.y }}
+          handle= ".float_chat_btn"
           onDrag={handleDrag}
           onStop={handleDragStop}
-          >
-          <div className='float_chat_btn_wrapper'>
-            <button className="float_chat_btn" onClick={toggleShowContent} onTouchEnd={toggleShowContent} ref={btnRef}>
+        >
+          <div className="float_chat_btn_wrapper">
+            <button
+              className="float_chat_btn"
+              onClick={toggleShowContent}
+              onTouchEnd={toggleShowContent}
+              ref={btnRef}
+            >
               <IoChatbubbleEllipsesSharp />
             </button>
           </div>
         </Draggable>
-        <div className='chat_content_container' ref={contentRef} 
+        <div
+          className="chat_content_container"
+          ref={contentRef}
           style={{
-            bottom : !isMobileDevice ? 115 - position.y : 0, 
+            bottom: !isMobileDevice ? 115 - position.y : 0,
             right: !isMobileDevice ? 75 - position.x : 0,
-            maxWidth : !isMobileDevice ? '300px' : '100vw',
-            maxHeight : !isMobileDevice ? '500px' : '100vh'
-            }}>
-          
+            maxWidth: !isMobileDevice ? '350px' : '100vw',
+            maxHeight: !isMobileDevice ? '600px' : '100vh',
+          }}
+        >
+          <UserChat 
+          toggleShowContent={toggleShowContent} 
+          showChat={showChat} 
+          setUnReadCount={setUnReadCount} 
+          unReadCount={unReadCount}
+          />
         </div>
       </div>
     </>
---
+++ src/components/chatting/UserChat.jsx
@@ -0,0 +1,129 @@
+import { MessageBox, Button, SystemMessage, Input } from 'react-chat-elements';
+import io from 'socket.io-client';
+import BASE_URL from '../../constants/baseurl';
+import { IoMdClose } from 'react-icons/io';
+import { useEffect, useRef, useState } from 'react';
+import { getUserInfo } from '../../util/getUserInfo';
+import useToast from '../../hooks/useToast';
+
+export default function UserChat({toggleShowContent, showChat, unReadCount, setUnReadCount}) {
+  const [isConnected, setIsConnected] = useState(false);
+  const [isAdminConnected, setIsAdminConnected] = useState(false);
+  const [inputMsg, setInputMsg] = useState('')
+  const [messages, setMessages] = useState([])
+  const [chatRoom, setChatRoom] = useState('')
+  const user = getUserInfo();
+  const socket = useRef(null); 
+
+  const handleSend = () => {
+    const message = {
+      text : inputMsg,
+      sender : user.id,
+      date : new Date(),
+    }
+    socket.current.emit('chatMessage', {message, chatRoomId : chatRoom});
+
+    setInputMsg('');
+  }
+
+  
+  const handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      handleSend();
+    }
+  }
+
+  useEffect(()=>{
+    if (showChat || !isConnected) {
+      setUnReadCount(0)
+    } else {
+      setUnReadCount(unReadCount+1)
+    }
+  },[messages.length])
+
+
+  const handleClick = () => {
+    socket.current = io(BASE_URL, { transports: ['websocket', 'polling'] });
+
+    socket.current.on('connect', () => {
+      console.log('소켓 서버 연결 성공');
+      const userData = {user_id : user.id || null, isAdmin : user.user_role || null}
+      socket.current.emit('connectedUser', userData)
+      setIsConnected(true);
+    });
+
+    socket.current.on('getChatRoomId', chatRoomId => {
+      setChatRoom(chatRoomId)
+      socket.current.emit('joinChatRoom', chatRoomId)
+      console.log(chatRoomId);
+    })
+    
+    socket.current.on('getPrevMessage', (prevMessages)=>{
+      setMessages([...prevMessages])
+    })
+
+    socket.current.on('chatMessage', (messages) => {
+      setMessages(prevMessages => [...prevMessages, messages])
+    })
+
+    socket.current.on('getConnectedSockets', sockets => {
+      if (sockets.filter(socketId=>socketId!==socket.current.id).length) {
+        setIsAdminConnected(true)
+      } else {
+        setIsAdminConnected(false)
+      }
+    })
+  };
+
+  return (
+    <>
+    <div className="chat_content_header">
+            <div className={`connect_dot ${isAdminConnected ? 'connected' : ''}`}></div>
+            <span className="header_title">1:1 문의</span>
+            <button className="close_btn" onClick={toggleShowContent}>
+              <IoMdClose />
+            </button>
+      </div>
+      <div className="chat_content_message_list">
+        {isConnected ? (
+          <div className="connected">
+            <SystemMessage
+              text={`채팅 서버에 연결되었습니다. 자유롭게 문의를 작성해주세요!`}
+            />
+            {messages.map((msg,i) => 
+            <MessageBox key={`chatMsg${i}`}
+              position={msg?.sender===user.id ? 'right' : 'left'}
+              type='text'
+              title={msg?.sender===user.id ? '' : 'id'}
+              avatar={msg?.sender===user.id ? '' : 's'}
+              date = {msg?.date}
+              // dateString={timeStr(msg?.date)}
+              text = {msg?.text}
+              maxWidth={50}
+            />
+            )}
+          </div>
+        ) : (
+          <div className="not_connected">
+            <button onClick={handleClick}>1:1 문의 연결</button>
+            <span>채팅 문의를 원하시면 위 버튼을 클릭해주세요 !</span>
+          </div>
+        )}
+      </div>
+      {isConnected ? <div className="chat_content_bottom">
+        <Input
+          onChange={e=>setInputMsg(e.target.value)}
+          value={inputMsg}
+          placeholder="메시지를 입력하세요"
+          rightButtons={
+            <Button  color="white" 
+                    backgroundColor="black" 
+                    text="전송"
+                    onClick={handleSend} />
+          }
+          onKeyPress={handleKeyPress}
+        />
+      </div> : null}
+    </>
+  );
+}
